d31dac9760c23fce426a9daa3ba3596149c1c7bb
landonbconnell <landonbconnell2001@gmail.com>
2024-11-13 16:15:42 +0000

    implemented generate_images.py and generated 100 images for demo

--- pipeline/steps/step_7/generate_images.py
+++ pipeline/steps/step_7/generate_images.py
@@ -4,7 +4,7 @@ from huggingface_hub import model_info
 import torch
 from PIL import Image
 
-def run(model_id, clusters):
+def run(model_id, clusters, num_generated_images_per_cluster=100):
     """
     Generate images from a text-to-image model on Hugging Face using the provided prompts.
     
@@ -15,7 +15,7 @@ def run(model_id, clusters):
     Returns:
     - images (list): A list of PIL Image objects generated from the prompts.
     """
-    generated_images_dir = f"./data/generated_images/{model_id}"
+    generated_images_dir = f"./data/generated_images/{model_id.replace('/', '_')}"
     os.makedirs(generated_images_dir, exist_ok=True)
 
     card_data = model_info(model_id).card_data.to_dict()
@@ -26,7 +26,7 @@ def run(model_id, clusters):
         return
     
     pipeline = None
-    if 'lora' in tags and 'base_model' in card_data.keys():
+    if 'base_model' in card_data.keys():
         base_model_id = card_data['base_model']
         
         print(f"Loading model {base_model_id} with LoRA weights from {model_id}...")
@@ -47,14 +47,20 @@ def run(model_id, clusters):
     for cluster_id, items in clusters.items():
         cluster_dir = os.path.join(generated_images_dir, f"cluster_{cluster_id}")
         os.makedirs(cluster_dir, exist_ok=True)
-        for item in items:
-            # careful - index may not exist
-            image_save_path = os.path.join(cluster_dir, f"generated_image_{item['index']}")
-            prompt = item['caption']
 
-            print(f"Generating image for prompt: '{prompt}'")
+        # Generate num_generated_images_per_cluster images for each cluster
+        for i in range(num_generated_images_per_cluster):
+            # Cycle through items by using the modulus operator
+            item = items[i % len(items)]
+            
+            # Construct the file path with a sequential index
+            image_save_path = os.path.join(cluster_dir, f"generated_image_{i}.png")
+            
+            # Generate image for the selected prompt
+            prompt = item['caption']
+            print(f"\nGenerating image for prompt: '{prompt}'")
             image = pipeline(prompt, num_inference_steps=25).images[0]
             image.save(image_save_path)
             print(f"Image saved to {image_save_path}")
 
-run("rjaiswal/sdxl-montresandco-model-lora", [])
+
